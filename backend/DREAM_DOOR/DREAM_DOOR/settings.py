fd509936c7881d85d910321f304613fbce3f05da
Grey Christensen <christensen.grey20@gmail.com>
2026-01-03 14:35:23 -0700

    Read SECRET_KEY and DEBUG from environment for Railway

--- backend/DREAM_DOOR/DREAM_DOOR/settings.py
+++ backend/DREAM_DOOR/DREAM_DOOR/settings.py
@@ -25,10 +25,13 @@ load_dotenv(BASE_DIR / ".env")
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gacpu0oikz&3njjer!!*z%s=b%ajs-0ocmzmwl_to2c(t!fv3g'
+SECRET_KEY = os.getenv(
+    'SECRET_KEY',
+    'django-insecure-gacpu0oikz&3njjer!!*z%s=b%ajs-0ocmzmwl_to2c(t!fv3g'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ["*"]
 
