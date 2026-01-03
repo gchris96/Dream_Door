e90cf9175cb3ec41b507c7bf6e2e8c69b27cc3fc
Grey Christensen <christensen.grey20@gmail.com>
2026-01-03 14:47:31 -0700

    debugging railway

--- backend/DREAM_DOOR/DREAM_DOOR/settings.py
+++ backend/DREAM_DOOR/DREAM_DOOR/settings.py
@@ -32,6 +32,7 @@ SECRET_KEY = os.getenv(
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
+print("DEBUG flag:", DEBUG)
 
 ALLOWED_HOSTS = ["*"]
 
